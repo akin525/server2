a7bf350a04ae4c54e431a0f354b1fdba5cac19be
HP <akinlabisamson15@gmail.com>
2023-01-24 08:57:10 +0100

    look

--- app/controllers/fund.controller.js
+++ app/controllers/fund.controller.js
@@ -18,45 +18,45 @@ exports.fund =  async (req, res) => {
             return res.status(200).send({status: "0", message: "Kindly login your account."});
         }
 
-        // if (req.body.amount < 0)
-        // {
-        //     return res.status(200).send({
-        //         status: "0",
-        //         message: "invalid transaction"
-        //     });
-        // }
-        //
-        //
-        //
-        //
-        // var options = {
-        //     'method': 'POST',
-        //     'url': 'https://api.budpay.com/api/v2/transaction/initialize',
-        //     'headers': {
-        //         'Authorization': 'Bearer sk_test_f5n6aovwjbqjo6ipwffwjjhg0btgwqge43fg8vm'
-        //     },
-        //     formData: {
-        //         'email': user.email,
-        //         'amount': req.body.amount,
-        //         'callback': 'youcallbackurl'
-        //     }
-        // };
-        // request(options, function (error, response) {
-        //     if (error) throw new Error(error);
-        //     var data=JSON.parse(response.body);
-        //     // console.log(data.success);
-        //     if (data.status=="true"){
-        //         console.log(data);
-        //
-        //         return   res.status(200).send({
-        //             status: "1",
-        //             data:data.data,
-        //             url:data.data.authoriza+tion_url,
-        //         });
-        //     }
-            res.status(200).send(user);
+        if (req.body.amount < 0)
+        {
+            return res.status(200).send({
+                status: "0",
+                message: "invalid transaction"
+            });
+        }
 
-        // });
+
+
+
+        var options = {
+            'method': 'POST',
+            'url': 'https://api.budpay.com/api/v2/transaction/initialize',
+            'headers': {
+                'Authorization': 'Bearer sk_test_f5n6aovwjbqjo6ipwffwjjhg0btgwqge43fg8vm'
+            },
+            formData: {
+                'email': user.email,
+                'amount': req.body.amount,
+                'callback': 'youcallbackurl'
+            }
+        };
+        request(options, function (error, response) {
+            if (error) throw new Error(error);
+            var data=JSON.parse(response.body);
+            // console.log(data.success);
+            if (data.status=="true"){
+                console.log(data);
+
+                return   res.status(200).send({
+                    status: "1",
+                    data:data.data,
+                    url:data.data.authoriza+tion_url,
+                });
+            }
+            // res.status(200).send(response.body);
+
+        });
 
         //
 
