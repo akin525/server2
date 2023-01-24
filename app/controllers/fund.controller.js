3dcb84257568332c062411fbd4265c099bddf10b
HP <akinlabisamson15@gmail.com>
2023-01-24 08:52:28 +0100

    look

--- app/controllers/fund.controller.js
+++ app/controllers/fund.controller.js
@@ -1,20 +1,11 @@
 const db = require("../models");
 const User = db.user;
-const bill= db.bill;
-const deposit=db.deposit;
 var request = require('request');
-const {response} = require("express");
-const {where} = require("sequelize");
-const { body, validationResult } = require('express-validator');
-const nodemailer = require("nodemailer");
 
 exports.fund =  async (req, res) => {
     const userid = req.body.userId;
-    var boy;
-    try {
 
-        let authorities = [];
-        var amount=req.body.amount;
+    try {
 
         const user = await User.findOne({
             where: {
@@ -27,45 +18,45 @@ exports.fund =  async (req, res) => {
             return res.status(200).send({status: "0", message: "Kindly login your account."});
         }
 
-        if (req.body.amount < 0)
-        {
-            return res.status(200).send({
-                status: "0",
-                message: "invalid transaction"
-            });
-        }
+        // if (req.body.amount < 0)
+        // {
+        //     return res.status(200).send({
+        //         status: "0",
+        //         message: "invalid transaction"
+        //     });
+        // }
+        //
+        //
+        //
+        //
+        // var options = {
+        //     'method': 'POST',
+        //     'url': 'https://api.budpay.com/api/v2/transaction/initialize',
+        //     'headers': {
+        //         'Authorization': 'Bearer sk_test_f5n6aovwjbqjo6ipwffwjjhg0btgwqge43fg8vm'
+        //     },
+        //     formData: {
+        //         'email': user.email,
+        //         'amount': req.body.amount,
+        //         'callback': 'youcallbackurl'
+        //     }
+        // };
+        // request(options, function (error, response) {
+        //     if (error) throw new Error(error);
+        //     var data=JSON.parse(response.body);
+        //     // console.log(data.success);
+        //     if (data.status=="true"){
+        //         console.log(data);
+        //
+        //         return   res.status(200).send({
+        //             status: "1",
+        //             data:data.data,
+        //             url:data.data.authoriza+tion_url,
+        //         });
+        //     }
+            res.status(200).send(user);
 
-
-
-
-        var options = {
-            'method': 'POST',
-            'url': 'https://api.budpay.com/api/v2/transaction/initialize',
-            'headers': {
-                'Authorization': 'Bearer sk_test_f5n6aovwjbqjo6ipwffwjjhg0btgwqge43fg8vm'
-            },
-            formData: {
-                'email': user.email,
-                'amount': req.body.amount,
-                'callback': 'youcallbackurl'
-            }
-        };
-        request(options, function (error, response) {
-            if (error) throw new Error(error);
-            var data=JSON.parse(response.body);
-            // console.log(data.success);
-            if (data.status=="true"){
-                console.log(data);
-
-                return   res.status(200).send({
-                    status: "1",
-                    data:data.data,
-                    url:data.data.authoriza+tion_url,
-                });
-            }
-            // res.status(200).send(response.body);
-
-        });
+        // });
 
         //
 
