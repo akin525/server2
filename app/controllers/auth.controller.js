554c6658262a08cf19e0347e8c1cafc0da239613
HP <akinlabisamson15@gmail.com>
2023-02-02 09:44:41 +0100

    look

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -205,6 +205,50 @@ exports.signin = async (req, res) => {
       authorities.push("ROLE_" + roles[i].name.toUpperCase());
     }
 
+    if (user.account_number1=="1"){
+
+      var option = {
+        'method': 'POST',
+        'url': 'https://integration.mcd.5starcompany.com.ng/api/reseller/virtual-account3',
+        'headers': {
+          'Authorization': 'mcd_key_yhij3dui0678iujk23hegwtfyu23dwky'
+        },
+        formData: {
+          'account_name': req.body.name,
+          'business_short_name': 'SAVEBILLS',
+          'uniqueid': user.username+Math.floor((Math.random() * 10000) + 1),
+          'email': user.email,
+          'dob': user.dob,
+          'address': user.address,
+          'gender': user.gender,
+          'phone': user.phone,
+          'webhook_url': 'https://server.savebills.com.ng/api/auth/run1'
+        }
+      };
+      request(option, function (error, response) {
+        if (error) throw new Error(error);
+        console.log(response.body);
+
+
+        var data=JSON.parse(response.body);
+        console.log(response.body);
+        console.log(data);
+        const objectToUpdate = {
+          account_number1: data.data.account_number,
+          account_name1: data.data.customer_name,
+        };
+
+        User.findAll({ where: { username: user.username}}).then((result) => {
+          if(result){
+            result[0].set(objectToUpdate);
+            result[0].save();
+          }
+        })
+
+      });
+
+
+    }
     // req.session.token = token;
 
     return res.status(200).send({
