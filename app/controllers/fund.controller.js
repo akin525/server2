f74d1d5b2e80751ea4b002d2bffb1429f0c42131
PIPER <akinlabi@gmail.com>
2023-09-26 13:32:56 +0100

    notification

--- app/controllers/fund.controller.js
+++ app/controllers/fund.controller.js
@@ -3,6 +3,7 @@ const User = db.user;
 const deposit=db.deposit;
 var request = require('request');
 const settings=db.settings;
+const charges=db.settings;
 
 exports.fund =  async (req, res) => {
     const userid = req.body.userId;
@@ -108,82 +109,69 @@ exports.fundverify =  async (req, res) => {
 
 
 
-        var options = {
-            'method': 'GET',
-            'url': 'https://sandbox-api-d.squadco.com/transaction/verify',
-            'headers': {
-                'Authorization': 'Bearer sandbox_sk_1e60156e0e029ec62daa87e91f5a3b0f1a0923246bec'
-            },
-
-            formData: {
-                transaction_ref:req.body.refid,
+        const options = {
+            method: 'GET',
+            url: `https://sandbox-api-d.squadco.com/transaction/verify/${req.body.refid}`,
+            headers: {
+                Authorization: 'Bearer sandbox_sk_1e60156e0e029ec62daa87e91f5a3b0f1a0923246bec'
             }
         };
+
         request(options, function (error, response) {
-            if (error) throw new Error(error);
-            var data=JSON.parse(response.body);
-            // console.log(data.success);
-            // if (data.status=="true"){
-                // console.log(data);
+            if (error) {
+                console.error('Error:', error);
+                return res.status(200).send({ status: '0', message: 'Request failed' });
+            }
 
-            // if (data.status ==200){
-            //     const amount = data.data.transaction_amount/100;
-            //
-            //     const totalamount=amount-charge.charges;
-            //
-            //     const allamount=parseInt(user.wallet)+totalamount;
-            //
-            //     if (data.data.transaction_status == "success"){
-            //
-            //
-            //         const insertcharges =charges.create({
-            //             username:user.username,
-            //             payment_ref:req.body.refid,
-            //             amount:charge.charges,
-            //             iwallet:user.wallet,
-            //             fwallet:parseInt(user.wallet)+amount,
-            //             status:"1",
-            //         });
-            //         const insertdeposit=deposit.create({
-            //             status:"1",
-            //             username:user.username,
-            //             payment_ref:refid,
-            //             amount:amount,
-            //             narration:"Amount be fund by Squad",
-            //             iwallet:user.wallet,
-            //             fwallet:parseInt(user.wallet)+parseInt(amount),
-            //         })
-            //         const objectToUpdate = {
-            //             wallet:allamount,
-            //         }
-            //
-            //         User.findAll({ where: { id: user.id}}).then((result) => {
-            //             if(result){
-            //                 result[0].set(objectToUpdate);
-            //                 result[0].save();
-            //             }
-            //         })
-            //
-            //
-            //     }else if(data.data.transaction_status == "failed"){
-            //
-            //         return   res.status(200).send({
-            //             status: "0",
-            //             message:data.data,
-            //         });
-            //     }
-            //
-            // }
+            try {
+                const data = JSON.parse(response.body);
 
-            // }
-            return res.status(200).send({
-                status: "1",
-                message: response.body,
-                body:fundrefid
-            });
+                if (data.status === 200) {
+                    const amount = data.data.transaction_amount / 100;
+                    const totalamount = amount - charge.charges;
+                    const allamount = parseInt(user.wallet) + totalamount;
 
+                    if (data.data.transaction_status === 'success') {
+                        // Create records in the database (assuming "charges" and "deposit" are models)
+                        charges.create({
+                            username: user.username,
+                            payment_ref: req.body.refid,
+                            amount: charge.charges,
+                            iwallet: user.wallet,
+                            fwallet: parseInt(user.wallet) + amount,
+                            status: '1'
+                        });
 
+                        deposit.create({
+                            status: '1',
+                            username: user.username,
+                            payment_ref: req.body.refid, // Assuming you meant req.body.refid here
+                            amount: amount,
+                            narration: 'Amount funded by Squad',
+                            iwallet: user.wallet,
+                            fwallet: parseInt(user.wallet) + parseInt(amount)
+                        });
+
+                        // Update the user's wallet amount in the database
+                        User.update(
+                            { wallet: allamount },
+                            { where: { id: user.id } }
+                        ).then(() => {
+                            // Handle the success of the database update if needed
+                        });
+                    } else if (data.data.transaction_status === 'failed') {
+                        return res.status(200).send({
+                            status: '0',
+                            message: data.data
+                        });
+                    }
+                }
+            } catch (e) {
+                console.error('Error parsing response:', e);
+                return res.status(200).send({ status: '0', message: 'Error parsing response' });
+            }
         });
+
 
         //
 
