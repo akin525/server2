8a9856cf90eb10d379c747b62faf3f9690178082
DELL <akinlabisamson15@gmail.com>
2024-07-25 23:16:33 +0100

    new sign up

--- app/controllers/chatapi.controller.js
+++ app/controllers/chatapi.controller.js
@@ -12,23 +12,38 @@ exports.chatapi =  async (req, res) => {
   const {senderId, recipientId}=req.body;
 
   try {
+    const { senderId, recipientId } = req.body;
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const offset = (page - 1) * limit;
 
     const history = await chat.findAll({
       where: {
         [Op.or]: [
-          {
-            senderId: senderId,
-            recipientId: recipientId,
-          },
-          {
-            senderId: recipientId,
-            recipientId: senderId,
-          }
+          { senderId, recipientId },
+          { senderId: recipientId, recipientId: senderId }
         ]
-      }
+      },
+      limit,
+      offset,
+      order: [['createdAt', 'DESC']] // Sort by createdAt, adjust as needed
     });
 
-    return res.status(200).send({status:1,data:{ message:history}});
+    return res.status(200).send({
+      status: 1,
+      data: {
+        message: history,
+        currentPage: page,
+        totalPages: Math.ceil(await chat.count({
+          where: {
+            [Op.or]: [
+              { senderId, recipientId },
+              { senderId: recipientId, recipientId: senderId }
+            ]
+          }
+        }) / limit)
+      }
+    });
   } catch (error) {
     return res.status(500).send({message: error.message});
   }
