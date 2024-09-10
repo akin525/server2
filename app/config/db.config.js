7f98826e8efa11ff884324fc414901f84cd0aa27
akina <akinlabisamson15@gmail.com>
2024-09-10 21:15:16 +0100

    reward

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,23 +1,10 @@
 require('dotenv').config();
 
-// module.exports = {
-//   HOST: process.env.DB_HOST,
-//   USER: process.env.DB_USER,
-//   PASSWORD: process.env.DB_PASSWORD,
-//   DB: process.env.DB_NAME,
-//   dialect: "mysql",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
-// };
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "savebills",
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
   dialect: "mysql",
   pool: {
     max: 5,
@@ -26,4 +13,17 @@ module.exports = {
     idle: 10000
   }
 };
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "",
+//   DB: "savebills",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
 
